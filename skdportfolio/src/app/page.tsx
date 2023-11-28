06a5c8c5acb7b53e470b5f205fde13ab25da56f0
siri-dhillon <sirpreetdhillon97@gmail.com>
2023-11-28 00:26:49 -0800

    setting up a next.js app for Portfolio website

---
+++ skdportfolio/src/app/page.tsx
@@ -0,0 +1,14 @@
+//import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+          <h1>Sirpreet Kaur Dhillon</h1>
+      </div>
+      <div>
+        <img src="Siri2.0.jpg" alt="Siri2.0" />
+      </div>
+    </main>
+  )
+}
